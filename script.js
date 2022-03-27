40e7b8c9c6b12ea27464da14d8268a3b387311e5
alex-lemos <75213249+alex-lemos@users.noreply.github.com>
2022-03-27 16:15:02 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// # GLOBAL
+
+let svg = document.querySelector(".svg__path");
+let btnShare = document.querySelector(".information-img");
+let containerShare = document.querySelector(".container-shared");
+
+// # EVENT
+
+btnShare.addEventListener("click", showContainerShare);
+
+// # FUNCTION
+
+function showContainerShare(){
+    svg.classList.toggle("svg__path--bg");
+    btnShare.classList.toggle("information-img--bg");
+    containerShare.classList.toggle("container-shared--hidden")
+}
+
+console.log(containerShare)
