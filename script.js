66fe2976c0b613dd27c9fcaf472c05c05d90a500
alex-lemos <75213249+alex-lemos@users.noreply.github.com>
2022-03-27 21:19:35 -0300

    add

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 
 let svg = document.querySelector(".svg__path");
 let btnShare = document.querySelector(".information-img");
+let btnShareMobile = document.querySelector(".mobile__bg");
 let containerShare = document.querySelector(".container-shared");
+let textInfo = document.querySelector(".text-information");
+let containerMobile = document.querySelector(".container-mobile");
 
 // # EVENT
 
 btnShare.addEventListener("click", showContainerShare);
+btnShareMobile.addEventListener("click", showContainerShare);
 
 // # FUNCTION
 
 function showContainerShare(){
-    svg.classList.toggle("svg__path--bg");
-    btnShare.classList.toggle("information-img--bg");
-    containerShare.classList.toggle("container-shared--hidden")
+    svg.classList.toggle("svg__path--bg"); //Fixa a cor do svg
+    btnShare.classList.toggle("information-img--bg");//Fixa a cor do bg
+
+    containerShare.classList.toggle("container-shared--hidden");
+    textInfo.classList.toggle("text-information--hidden");
+    containerMobile.classList.toggle("container-mobile--hidden");
 }
